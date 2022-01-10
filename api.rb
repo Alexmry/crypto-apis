fcf0babaa5d3125e91313e193bba33058dde996f
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-10 17:17:50 +0000

    added the wrapper class

--- api.rb
+++ api.rb
@@ -1,12 +1,28 @@
 require './lib/base.rb'
 require './lib/coinbase.rb'
-require './lib/coingecko.rb'
-require './lib/coinpaprika.rb'
-require './lib/coincap.rb'
-require './lib/cryptocompare.rb'
+require './lib/coin_gecko.rb'
+require './lib/coin_paprika.rb'
+require './lib/coin_cap.rb'
+require './lib/crypto_compare.rb'
 
 module Api
+    class Wrapper < Base
+        %w[
+            coinbase
+            coin_gecko
+            coin_paprika
+            coin_cap
+            crypto_compare
+        ].each do |api|
+            define_method(api.to_sym) do
+                Object.const_get("Api::#{klassify(api)}").new(@options).request
+            end
+        end
 
+        def klassify(name)
+            name.split('_').collect(&:capitalize).join
+        end
+    end
 end
 
 
