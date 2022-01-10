b5b411fd462db0ea76a1d677026d7f939bf44128
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-10 16:24:05 +0000

    added coingecko api and updated the api file

--- api.rb
+++ api.rb
@@ -1,5 +1,6 @@
 require './lib/base.rb'
 require './lib/coinbase.rb'
+require './lib/coingecko.rb'
 
 module Api
 
--- lib/coingecko.rb
+++ lib/coingecko.rb
@@ -5,7 +5,7 @@ module Api
         end
 
         def parsed_response(response)
-            response.dig('data', 'amount')
+            response.dig('tickers').first.dig('last')
         end
 
         def url
