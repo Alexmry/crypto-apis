30c1130a6f77cc66209cf86e6fd052d62bc048cf
Alexandre Morey <alexander006_9@hotmail.com>
2022-01-10 16:55:39 +0000

    added cryptocompare api and updated the api file

--- api.rb
+++ api.rb
@@ -3,6 +3,7 @@ require './lib/coinbase.rb'
 require './lib/coingecko.rb'
 require './lib/coinpaprika.rb'
 require './lib/coincap.rb'
+require './lib/cryptocompare.rb'
 
 module Api
 
---
+++ lib/cryptocompare.rb
@@ -0,0 +1,16 @@
+module Api
+    class CryptoCompare < Base
+        def request
+            super(:get, url)
+        end
+
+        def parsed_response(response)
+            response.dig('USD').to_f
+        end
+
+        def url
+            # "https://min-api.cryptocompare.com/data/price?fsym=btc&tsyms=usd"
+            "https://min-api.cryptocompare.com/data/price?fsym=#{@options[:symbol]}&tsyms=usd"
+        end
+    end
+end
